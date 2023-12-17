0c8d09373bbac08d0eb5bb999564c34eb4d66cfe
Bryan S <Bryan.serino0513@gmail.com>
2023-12-17 13:33:41 -0500

    new file:   WeatherPy/.gitignore 	modified:   WeatherPy/WeatherPy.ipynb 	new file:   WeatherPy/__pycache__/api_keys.cpython-310.pyc 	modified:   WeatherPy/api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8ef1683657e3c852aa13668d958df88c"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "c5edab6b5e044117b2c4530e1202d842"
